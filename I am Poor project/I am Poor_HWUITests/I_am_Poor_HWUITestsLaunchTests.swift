93272657c0d8523737fae606a6a320c5a7dc6de6
William101102 <140888893+William101102@users.noreply.github.com>
2023-08-10 23:06:22 -0700

    Add files via upload

---
+++ I am Poor project/I am Poor_HWUITests/I_am_Poor_HWUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  I_am_Poor_HWUITestsLaunchTests.swift
+//  I am Poor_HWUITests
+//
+//  Created by William Huang on 2023/7/29.
+//
+
+import XCTest
+
+final class I_am_Poor_HWUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
---
+++ I am Rich Project/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by William Huang on 2023/7/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
