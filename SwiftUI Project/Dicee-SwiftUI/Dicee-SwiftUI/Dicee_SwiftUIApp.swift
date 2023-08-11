8fca604879b4b822a98fd80828f6af54b74d66b5
William101102 <140888893+William101102@users.noreply.github.com>
2023-08-10 23:37:19 -0700

    Add files via upload

---
+++ SwiftUI Project/Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by William Huang on 2023/8/9.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
